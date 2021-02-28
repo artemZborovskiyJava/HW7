9b24b42eab292f0e9cfaa3c45aacc215b446524a
artemZborovskiyJava <artem_zborovskiy@icloud.com>
2021-02-28 14:31:41 +0200

    second task fix

--- Main.java
+++ Main.java
@@ -11,9 +11,6 @@ public class Main {
 	    */
 
         int[] arr = new int[MAXSUMOFTHREENUMBERS];
-        for(int i = 0; i < 28; i++) {
-            arr[i] = 0;
-        }
         for(int i = 0; i < 10; i++) {
             for(int j = 0; j < 10; j++) {
                 for(int k = 0; k < 10; k++) {
